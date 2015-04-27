d7c73012eb6de84bb93d40814f5c819d904166ca
Aniket Lawande <anlawande@gmail.com>
2015-04-27 12:08:21 -0400

    Added distribution scripts and results

--- Scraper/commits.js
+++ Scraper/commits.js
@@ -1,4 +1,5 @@
 var LINQ = require('node-linq').LINQ;
+var Stats = require('fast-stats').Stats;
 var fs = require("fs");
 
 var upperThreshold = 60;
@@ -45,5 +46,46 @@ for(var i = 0; i < commitsArr.length; i++) {
 	projectObj["P" + projectNum++] = commitPerc;
 }
 
+console.log("Commit Distribution Bad Smell")
 console.log(projectObj);
 
+projectNum = 1;
+for(var i = 0; i < commitsArr.length; i++) {
+	var commits = new LINQ(commitsArr[i]);
+	var obj = {};
+	var valuesArr = [];
+	
+	var result = commits.Select(function(x) {return x.weeks}).Map(function(x) {
+		for(var j = 0; j < x.length; j++) {
+			if(!obj[x[j].w])
+				obj[x[j].w] = 0;
+			obj[x[j].w] += x[j].c;
+		}
+	})
+	
+	var weekNum = 1;
+	var obj2 = {};
+	for(var a in obj) {
+		obj2["Week " + weekNum++] = obj[a];
+		valuesArr.push(obj[a]);
+	}
+	var s = new Stats().push(valuesArr);
+	projectObj["P" + projectNum] = {
+		dist: obj2,
+		mean: parseFloat(s.amean().toFixed(2)),
+		stddev: parseFloat(s.stddev().toFixed(2))
+	}
+	
+	var project = projectObj["P" + projectNum];
+	project.distBad = [];
+	weekNum = 1;
+	for(var j in project.dist) {
+		var num = projectObj["P" + projectNum].dist[j];
+		project.distBad["Week " + weekNum++] = num > (project.mean + project.stddev) || num < (project.mean - project.stddev);
+	}
+	
+	projectNum++;
+}
+
+console.log("Commit Weekly Distribution");
+console.log(projectObj);
